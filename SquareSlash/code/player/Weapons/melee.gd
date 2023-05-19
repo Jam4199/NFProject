ae471a79dbca24ef6dd3b1b4414f5b0a6995c07f
Jam4199 <125315614+Jam4199@users.noreply.github.com>
2023-05-20 06:30:25 +0800

    state stuff

--- SquareSlash/code/player/Weapons/melee.gd
+++ SquareSlash/code/player/Weapons/melee.gd
@@ -1,1 +1,31 @@
 extends State
+
+
+@export var charge_time : float = 1.5
+
+var current_charge : float = 0
+var full_charged : bool = false
+
+
+func proc(delta : float):
+
+	current_charge += delta
+	if current_charge >= charge_time:
+		full_charge():
+	return
+
+func entered():
+	charge_time = 0
+	full_charged = false
+	
+func full_charge():
+	full_charged = true
+
+
+func input(command: String):
+	match command:
+		"melee_released":
+			if full_charged:
+				print("do the charge_attack")
+			else:
+				controller.transition("idle")
